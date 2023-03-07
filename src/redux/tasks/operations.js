9a7c01e3a1e0291fcdd53ab6ebd7b62384524fbc
Natasha <natkamonte1992@gmail.com>
2023-03-07 19:50:47 +0200

    .

--- src/redux/tasks/operations.js
+++ src/redux/tasks/operations.js
@@ -1,6 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+axios.defaults.baseURL = 'https://goit-task-manager.herokuapp.com/';
 // GET @ /tasks
 export const fetchTasks = createAsyncThunk(
   'tasks/fetchAll',
